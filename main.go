ea27ac419125b36dab7f31e27012865c6873532f
Edgar Albalate <dev.gardo@gmail.com>
2018-10-26 10:20:55 +0200

    Added sprite drawing and macOS mojave Update fix

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"image"
+	_ "image/png"
+	"os"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
+
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
-		Bounds: pixel.R(0, 0, 1024, 768),
+		Bounds: pixel.R(0, 0, 640, 480),
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
@@ -17,7 +34,19 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Skyblue)
+	pic, err := loadPicture("hiking.png")
+	if err != nil {
+		panic(err)
+	}
+
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
+	win.Clear(colornames.Greenyellow)
+
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+
+	// * Mojave fix
+	win.SetPos(win.GetPos().Add(pixel.V(0, 1)))
 
 	for !win.Closed() {
 		win.Update()
